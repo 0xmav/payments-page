7e1f0fb382b8bb983778d408edafa79338b14e46
0xmav <vladislav.f.denisov@gmail.com>
2023-06-28 21:53:56 +0400

    Corrected the CC

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,9 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-top p-24">
             <div className="py-12 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
                 <Image className="relative rounded-full dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" src="/cc-logo.jpg" alt="Collector's Corner Logo" width={120} height={37} priority />
-                <p className="text-3xl font-semibold px-14">Collector's corner Premium</p>
+                <p className="text-3xl font-semibold px-14">Collector's Corner Premium</p>
             </div>
 
-            {/* <div className="space-x-5 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"> */}
             <div className="grid gap-2 text-left lg:grid-cols-3">
                 <PaymentCard planName="Founder" price="4200" renew="year" list={["Weekly 1 on 1 sessions to cover personal portfolio"]}></PaymentCard>
                 <PaymentCard planName="Annual" price="1000" renew="year" list={["20% discount"]}></PaymentCard>
